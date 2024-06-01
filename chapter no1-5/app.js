ddf78b2562c6bb627f5eddeb62db9f11df19a36d
webdeveloperbysk <aah.kashif@gmail.com>
2024-06-01 23:52:47 +0500

    JS assignment

---
+++ chapter no1-5/app.js
@@ -0,0 +1,217 @@
+
+// Chapter no 1
+
+//  (Q 1)
+
+ alert("Welcome");
+
+// ====== Chapter no 1 (Q 2)======
+ alert("Error! Please enter a valid password.")
+
+// ====== Chapter no 1 (Q 3)======
+
+ alert("Welcome to JS Land...\n Happy Coding")
+
+// ====== Chapter no 1(Q 4)======
+
+ alert("Welcome to JS Land...");
+
+//  wait for 3 seconds
+
+// setTimeout(function() {
+//  Display second message
+ alert("Happy Coding!");
+//  }, 3000);
+
+// ======Chapter no 1 (Q 5)======
+
+  alert("Hello...I can run JS through my web browser's console");
+
+// Q no 6 & 7 in practice project 3
+
+
+
+
+
+
+//  ===Chap no 2===
+//  Q No 1. Declare a variable called username.
+ var userName;  
+
+//  Q No 2.Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+ myName = "Subul Kashif";
+ console.log(myName);
+
+// Q No 3.Write script to
+// a) Declare a JS variable, titled message.
+ var message;
+
+//  b) Assign “Hello World” to variable message
+ message = "Hello World"
+//  c) Display the message in alert box.
+ alert(message);
+
+
+// Q no 4.
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// student's bio data
+//  var studentName, age;
+
+  studentName = "Jhone Doe";
+
+  age = "15 years old";
+
+ course = "Certified Mobile Application Development";
+
+// // Displaying the data in alert boxes
+
+ alert("Jhone Doe");
+  alert("15 years old");
+
+  alert("Certified Mobile Application Development");
+
+
+//  Q No 5.
+// Write a script to display the following alert using one JS
+//  variable:
+ alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+
+
+
+
+
+//  Q No 6.
+// Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+  var email = "abc@example.com";
+alert("My eamil address is abc@example.com");
+
+
+
+
+//  Q No 7.
+// Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+ var book ="I am trying to learn from the Book A smarter way to learn Javascript";
+ alert("I am trying to learn from the Book A smarter way to learn Javascript");
+
+
+
+// Q No 8.
+ document.write("Yah! I can write HTML content through Java Script");
+
+
+// Q No 9.
+// Store following string in a variable and show in alert and
+// browser through JS
+ var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+ alert(myString);
+
+
+
+
+
+
+
+
+
+
+
+// Chapter No 3
+
+// Question No 1
+
+
+ var age;
+
+alert("I am 15 years old");
+
+// Question No 2
+
+ var visitorVisited;
+ alert("You have visited this site 14 times")
+
+// Question No 3
+ var birthYear = 1990;
+ document.write("My birth year is 1990 <br> Data type of my declared variable is number")
+
+
+// Question No 4
+ var visitorName = "John Doe";
+ var productTitle = "T-shirt";
+ var quantity = 5;
+ var message = ("John Doe ordered 5 T-shirt(s) on XYZ Clothing store.")
+
+ document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store.")
+
+
+
+
+
+// Chapter No 4
+
+// Question no 1
+ var visitorName = "Subul Kashif", productTitle = "T-shirt", quantity = 5;
+ console.log(visitorName)
+  console.log(productTitle)
+  console.log(quantity)
+
+
+//  Question no 2
+
+// Legal Variable Names
+
+'firstName'
+'_age'
+'$price'
+'totalAmount'
+'user3'
+
+
+// Illegal Variable Names
+'3user'
+'first-name'
+'var'
+'total amount'
+'@home'
+
+// Question no 3
+
+document.write("Rules for naming JS variables<br>Variable names can only contain ______, ______,______ and ______.<br>For example $my_1stVariable <br> Variables must begin with a ______, ______ or_____. For example $name, _name or name<br>Variable names are case _________<br>Variable names should not be JS _________")
+
+
+
+
+
+
+
+
+
+
+// Chapter No 5
+
+// Question No 1
+
+const Num1 = 3;
+const Num2 = 5;
+
+// add two numbers
+const Sum = Num1 + Num2;
+
+// console.log('The sum of ' + num1 + 'and ' + num2  + ' is:' + sum);
+
+
+// Question No 2
+const num1 = 3;
+const num2 = 5;
+
+// sub two numbers
+const sum = num1 - num2;
